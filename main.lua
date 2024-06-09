f420fdd0aa09cf3be621fae01eeedec534d95d53
notexplosive <thenotexplosive@gmail.com>
2024-06-09 15:04:25 -0700

    initial commit

---
+++ main.lua
@@ -0,0 +1,28 @@
+local main = {}
+
+function main.onStart()
+
+end
+
+
+function main.onInput(input)
+
+end
+
+
+function main.onMove(move)
+
+end
+
+
+function main.onEntityDestroyed(entity)
+
+end
+
+
+function main.onUpdate(dt)
+
+end
+
+return main
+
